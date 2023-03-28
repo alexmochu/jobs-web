e934603137a6a7e682f5e8947b7886cc8d241ee6
alexmochu <mochualex4@gmail.com>
2023-03-28 18:13:46 +0300

    update login

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -33,22 +33,28 @@ export default function Login() {
 
   return (
     <Fragment>
-      <div>
-        {/* {token ? (
-        <>
-          <p>You are logged in.</p>
-          <p>The message: {message}</p>
-          <button onClick={handleLogout}>Login out</button>
-        </>
-        ) : ( */}
-        <>
+          {/* <LoginGithub
+            clientId={clientId}
+            onSuccess={handleLoginSuccess}
+            onFailure={handleLoginFailure}
+            redirectUrl={'http://localhost:5173/login/github/authorized'}
+          /> */}
+          <div className="relative">
+    <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+<div className="blur-[106px] h-32 bg-gradient-to-br from-cyan-800 to-purple-800 dark:from-blue-700"></div>
+        <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-800 to-sky-600 dark:to-indigo-600"></div>
+    </div>
+    <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+        <div className="relative pt-36 ml-auto">
+            <div className="lg:w-2/3 text-center mx-auto">
           <label className="block">
-            <button type='submit' className="block text-sm font-medium text-slate-700">Login</button>
-            <span className="block text-sm font-medium text-slate-700">Dont have an account? <Link to='/signup'>Register</Link></span>
+            <h1 className='text-4xl font-bold tracking-tight mb-2 text-gray-900'>Login</h1>
+            <span className="block text-sm font-medium text-slate-700">Dont have an account? <Link className='text-indigo-500' to='/signup'>Register</Link></span>
           </label>
+                <div className="mt-10 flex flex-wrap justify-center gap-y-4 gap-x-6">
           <Form method="post" id="login-form">
-            <p>
-              <span>Username</span>
+            <label className="block">
+              <span className="block text-left text-sm font-medium text-slate-700">Username</span>
               <input
                 placeholder="Username"
                 aria-label="Username"
@@ -56,10 +62,16 @@ export default function Login() {
                 name="username"
                 value={userState.username}
                 onChange={onChange}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                      invalid:border-pink-500 invalid:text-pink-600
+                      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                    "
               />
-            </p>
-            <p>
-              <span>Password</span>
+            </label>
+            <label className="block mt-2">
+              <span className="block text-left text-sm font-medium text-slate-700">Password</span>
               <input
                 placeholder="Password"
                 aria-label="Password"
@@ -67,171 +79,32 @@ export default function Login() {
                 name="password"
                 value={userState.password}
                 onChange={onChange}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                      invalid:border-pink-500 invalid:text-pink-600
+                      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                    "
               />
-            </p>
-            <p>
-              <button type="button" onClick={onLogin}>Save</button>
-              <button type="button">Cancel</button>
-            </p>
+            </label>
             <label className="block">
-              <span className="block text-sm font-medium text-slate-700">Cant rememeber password? <Link to='/forgot-password'>Recover it.</Link></span>
+              <div className="mt-5 mb-4">
+                <button type="button" onClick={onLogin} className="bg-indigo-500 text-gray-100 pt-2 pb-2 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg">
+                                    Log In
+                                </button>
+                            </div>
+            </label>
+            <label className="block">
+              <span className="block text-sm font-medium text-slate-700">Cant rememeber password? <Link className='text-indigo-500' to='/forgot-password'>Recover it.</Link></span>
             </label> 
-          </Form>
-          {/* <LoginGithub
-            clientId={clientId}
-            onSuccess={handleLoginSuccess}
-            onFailure={handleLoginFailure}
-            redirectUrl={'http://localhost:5173/login/github/authorized'}
-          /> */}
-          {/* {message && <p>{message}</p>} */}
-        </>
-        {/* )} */}
-      </div>
+          </Form>                  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     </Fragment>
-
-
-    // <>
-    // <Fragment>
-    //   <NavBar navigation={Navs} />
-    //   <main>
-    //     <div className='bg-white'>
-    //       <div>
-    //         {/* Mobile filter dialog */}
-    //         {/* <MobileForm
-    //           filters={filters}
-    //           mobileFiltersOpen={mobileFiltersOpen}
-    //           setMobileFiltersOpen={setMobileFiltersOpen}
-    //         /> */}
-    //         <main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-    //           <div className='flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6'>
-    //             <h1 className='text-4xl font-bold tracking-tight text-gray-900'>
-    //              Settings
-    //             </h1>
-    //             {/* <Header sortOptions={sortOptions} setMobileFiltersOpen={setMobileFiltersOpen} /> */}
-    //           </div>
-    //           <section aria-labelledby='products-heading' className='pt-6 pb-24'>
-    //             <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
-    //               {/* Filters */}
-    //               {/* <Form filters={filters} /> */}
-    //               {/* Product grid */}
-
-
-    //   <div id="sidebar">
-    //       <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
-    //       <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-    //           {contacts.length ? (
-    //           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-    //                           {contacts.map((contact) => (
-
-    //             <li  key={contact.id}>
-    //               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{contact.name}</a>
-    //             </li>
-    //               ))}
-    //           </ul>  ) : (
-    //         <p>
-    //           <i>No contacts</i>
-    //         </p>
-    //       )}
-    //       </div>
-    //             <nav>
-
-    //       {contacts.length ? (
-    //         <ul>
-    //           {contacts.map((contact) => (
-    //             <li key={contact.id}>
-    //                                   <NavLink
-    //                 to={`contacts/${contact.id}`}
-    //                 className={({ isActive, isPending }) =>
-    //                   isActive
-    //                     ? 'active'
-    //                     : isPending
-    //                     ? 'pending'
-    //                     : ''
-    //                 }
-    //               >
-    //               <Link to={`contacts/${contact.id}`}>
-    //                 {contact.first || contact.last ? (
-    //                   <>
-    //                     {contact.first} {contact.last}
-    //                   </>
-    //                 ) : (
-    //                   <i>No Name</i>
-    //                 )}{' '}
-    //                 {contact.favorite && <span>★</span>}
-    //               </Link></NavLink>
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       ) : (
-    //         <p>
-    //           <i>No contacts</i>
-    //         </p>
-    //       )}
-    //     </nav>
-    //     <div>
-    //       <Form id="search-form" role="search">
-    //         <input
-    //           id="q"
-    //                         className={searching ? 'loading' : ''}
-    //           aria-label="Search contacts"
-    //           placeholder="Search"
-    //           type="search"
-    //           name="q"
-    //           defaultValue={q}
-    //           // value={query}
-    //           onChange={(e) => {
-    //                             const isFirstSearch = q == null;
-    //             submit(e.currentTarget.form, {
-    //               replace: !isFirstSearch,
-    //             });
-    //             // setQuery(e.target.value)
-    //           }}
-    //         />
-    //         <div
-    //           id="search-spinner"
-    //           aria-hidden
-    //           hidden={!searching}
-    //         />
-    //         <div
-    //           className="sr-only"
-    //           aria-live="polite"
-    //         ></div>
-    //       </Form>
-    //       <Form method="post">
-    //         <button type="submit">New</button>
-    //       </Form>
-    //     </div>
-    //     <nav>
-    //       <ul>
-    //         <li>
-    //           <Link to={'contacts/1'}>Your Name</Link>
-    //         </li>
-    //         <li>
-    //           <Link to={'contacts/2'}>Your Friend</Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
-
-
-    //               <div className='lg:col-span-3'>
-    //                 <div className='h-96 rounded-lg border-4 border-dashed border-gray-200 lg:h-full'>
-    //                   {/* <List plans={plans} selected={selected} setSelected={setSelected} jobs={jobs} /> */}
-    //                         <div id="detail"         className={
-    //       navigation.state === 'loading' ? 'loading' : ''
-    //     }>
-    //     <Outlet />
-    //   </div>
-    //                 </div>
-    //                 {/* /End replace */}
-    //               </div>
-    //             </div>
-    //           </section>
-    //         </main>
-    //       </div>
-    //     </div>
-    //   </main>
-    // </Fragment>
-    // </>
   );
 }
