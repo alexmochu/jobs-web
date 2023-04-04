139bccc3ab1a5a0a9698174b06a92be60647d978
alexmochu <mochualex4@gmail.com>
2023-04-04 15:12:48 +0300

    make the checkbox clickable

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -17,10 +17,19 @@ const List = ({ plans, selected, setSelected, jobs}) => {
             }
           >
             {() => (
-              <>
-                <div className='w-full items-center grid grid-cols-10'>
+                <div className='w-full grid grid-cols-10'>
                   <div className='shrink-0 text-gray-800'>
-                    <StopIcon className='block h-6 w-6' aria-hidden='true' />
+                    {/* <StopIcon className='block h-6 w-6' aria-hidden='true' /> */}
+                    <input
+                        // id={`filter-${section.id}-${optionIdx}`}
+                        // name={`${section.id}[]`}
+                        id='new id'
+                        name='checkbox'
+                        // defaultValue={option.value}
+                        type='checkbox'
+                        // defaultChecked={option.checked}
+                        className='h-4 w-4 rounded border-gray-400 text-indigo-600 focus:ring-indigo-500'
+                      />
                   </div>
                   <div className='items-left col-span-7'>
                     <div className='text-sm'>
@@ -28,19 +37,20 @@ const List = ({ plans, selected, setSelected, jobs}) => {
                         <span>
                           <CalendarIcon className='mr-1 h-5 w-5 text-gray-800' aria-hidden='true' />
                         </span>{' '}
-                        <span>{'Job Dates'}</span>{' '}
+                        <span>{'1st April 2023'}</span>{' '}
                         <span className='mr-1.5 ml-1.5' aria-hidden='true'>
                           &middot;
                         </span>
-                        {'Role'}
+                        {'Senior Javascript Software Engineer'}
                       </RadioGroup.Label>
-                      <RadioGroup.Description as='span' className={`inline ${'text-gray-800'}`}>
-                        <span>{'Summary'}</span>
+                      <RadioGroup.Description as='span' className={`flex mt-2 pr-3 ${'text-gray-800'}`}>
+                        <span>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore tapasas cupiditate...'
+                        }</span>
                       </RadioGroup.Description>
                     </div>
                   </div>
                   <Menu as='div' className='relative inline-block text-left'>
-                    <div className='shrink-0 text-gray-800'>
+                    <div className='shrink-0 grid text-gray-800'>
                       <Menu.Button className='inline-flex w-full justify-center rounded-md bg-gray-800 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
                         View
                         <ArrowTopRightOnSquareIcon
@@ -51,7 +61,6 @@ const List = ({ plans, selected, setSelected, jobs}) => {
                     </div>
                   </Menu>
                 </div>
-              </>
             )}
           </RadioGroup.Option>
         ))}
