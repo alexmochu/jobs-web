3647fac117200ad58ba396dbc2071144aeabd1ea
alexmochu <mochualex4@gmail.com>
2023-04-09 14:49:43 +0300

    switch API url based on environment

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 // localhost --> http://127.0.0.1:5000/
 // production --> https://jobs.kejanigarage.com/
+
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000/'
+
 var axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
     'header-access-token': localStorage.getItem('headerAccessToken'),
