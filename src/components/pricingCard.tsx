88cacfb78cb2b2a4e182b2e0cf35a913af979051
alex.mochu <mochualex4@gmail.com>
2023-05-08 01:07:52 +0300

    update pricing card

--- src/components/pricingCard.tsx
+++ src/components/pricingCard.tsx
@@ -3,19 +3,21 @@ export function PricingCard({price}) {
     // let i = 0
     return (
       <div>
-        <div className="flex min-h-[428px] w-[320px] flex-col rounded border-4">
-          <h2 className="mt-8 ml-7 text-xs text-black font-normal">{price.package}</h2>
+        <div className="flex min-h-[428px] w-[320px] flex-col rounded-2xl border-2 border-indigo-500">
+          <h2 className="mt-8 ml-7 text-lg text-indigo-500 font-bold">{price.package}</h2>
           <div className="mb-4 ml-7 flex justify-between mr-7 items-end font-black">
-            <div className="font-normal text-3xl text-black">${price.fee}</div>
+            <div className="font-normal text-3xl text-black">${price.fee}/month</div>
           </div>
           <hr className="border border-[#2a2b2b]"/>
-          <ul className="mb-7 min-h-[70px] ml-7 mr-7 flex flex-col gap-y-2 text-black">
+          <ul className="mb-7 min-h-[70px] ml-7 mr-7 flex flex-col gap-y-2 text-black pt-10">
             {price.list.map(item => (
-              <li key={item}>{item}</li>
+              <li key={item}>
+                <svg class="h-5 w-5 inline-block mr-2" fill="currentColor" viewBox="0 0 20 20"><path d="M15.707,6.293c0.391,0.391,0.391,1.023,0,1.414l-6,6c-0.391,0.391-1.023,0.391-1.414,0l-3-3c-0.391-0.391-0.391-1.023,0-1.414s1.023-0.391,1.414,0l2.293,2.293l5.293-5.293C14.684,5.902,15.316,5.902,15.707,6.293z"></path></svg>
+                {item}</li>
             ))}
           </ul>
           <button
-            className={'rounded-full bg-indigo-500 mb-7 ml-7 mr-7 h-10 text-lg font-medium text-white hover:bg-white'}
+            className={'rounded-full bg-indigo-500 mb-7 ml-7 mr-7 h-10 text-lg font-medium text-white'}
             >
             Subscribe
           </button>
--- src/routes/pricing.jsx
+++ src/routes/pricing.jsx
@@ -6,32 +6,19 @@ export default function Pricing() {
   return (
     <>
     <Fragment>
-      {/* <main>
-        <div className='bg-white'>
-          <div>
-            <main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-              <div className='flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900'>
-                  Pricing
-                </h1>
-              </div>
-            </main>
-          </div>
-        </div>
-      </main> */}
           <div className='flex flex-col min-h-screen items-center h-full p-4 m-0'>
-            <div className='pt-24 pb-6'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900'>
+            <div className='pt-36 pb-6'>
+                <h1 className='text-6xl font-bold tracking-tight text-center text-gray-900'>
                   Pricing Plans
                 </h1>
-                <h3>Get 2 months for free by subscribing yearly!</h3>
-                        <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
-            <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" role="tablist">
+                <h3 className='text-center text-lg mt-5'>Get 2 months for free by subscribing yearly!</h3>
+                        <div className="mb-4">
+            <ul className="flex flex-wrap text-lg mt-10 font-medium text-center" role="tablist">
                 <li className="mr-2" role="presentation">
-                    <button className="inline-block p-4 border-b-2 rounded-t-lg" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
+                    <button className="inline-block p-4 border-b-2 rounded-t-lg bg-indigo-500 text-white w-[8rem]" type="button" role="tab" aria-controls="monthly" aria-selected="false">Monthly</button>
                 </li>
                 <li className="mr-2" role="presentation">
-                    <button className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" type="button" role="tab" aria-controls="yearly" aria-selected="false">Yearly</button>
+                    <button className="w-[8rem] inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" type="button" role="tab" aria-controls="yearly" aria-selected="false">Yearly</button>
                 </li>
             </ul>
         </div>
