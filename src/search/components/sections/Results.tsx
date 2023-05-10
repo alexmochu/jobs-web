e4f248dc073628ac9c57a95892832794572756e9
alex.mochu <mochualex4@gmail.com>
2023-05-10 20:42:50 +0300

    results view

---
+++ src/search/components/sections/Results.tsx
@@ -0,0 +1,20 @@
+import { ResultsCard } from '../../../components/ResultsCard'
+
+export const Results  = () => {
+    return(
+        <div className='grid grid-cols-3 gap-4 mt-10'>
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+        </div>
+    )
+}
