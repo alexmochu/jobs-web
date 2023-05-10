51e5196157aaf8c217635bfb19d303ee4c4dc1ef
alex.mochu <mochualex4@gmail.com>
2023-05-10 20:40:16 +0300

    job tracker ui

---
+++ src/tracker/tracker.tsx
@@ -0,0 +1,11 @@
+import { TrackerDashboard } from './components/sections';
+
+function Tracker() {
+  return (
+    <div className="paper inline mx-auto">
+      <TrackerDashboard />
+    </div>
+  );
+}
+
+export default Tracker;
