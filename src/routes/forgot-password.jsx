bdaf33b719aceea9163f8d171544a46478385f99
alex.mochu <mochualex4@gmail.com>
2023-05-10 14:48:40 +0300

    remove background

--- src/routes/forgot-password.jsx
+++ src/routes/forgot-password.jsx
@@ -5,12 +5,12 @@ export default function ForgotPassword() {
   return (
     <Fragment>
    <div className="relative" id="home">
-    <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+    {/* <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
 <div className="blur-[106px] h-32 bg-gradient-to-br from-cyan-800 to-purple-800 dark:from-blue-700"></div>
         <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-800 to-sky-600 dark:to-indigo-600"></div>
-    </div>
+    </div> */}
     <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-        <div className="relative mt-20 ml-auto">
+        <div className="relative mt-60 ml-auto">
             <div className="lg:w-2/3 text-center mx-auto">
           <label className="block">
             <h1 className='text-4xl font-bold tracking-tight mb-2 text-gray-900'>Recover password</h1>
