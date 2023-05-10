4ffd03451554ec98b49920fc991906a557f2b17c
alex.mochu <mochualex4@gmail.com>
2023-05-10 22:23:17 +0300

    update navbar

--- src/landingpage/AppHeader.jsx
+++ src/landingpage/AppHeader.jsx
@@ -1,8 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Fragment } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { userState } from '../main'
+import { classNames } from '../components/common'
+import Queries from '../api/queries'
 
 const AppHeader = () => {
   const { value, setValue } = userState()
+    const navigate = useNavigate()
+  const onLogout = async () => {
+    await Queries.logout()
+    return navigate('/login')
+  }
 
   const isAuthenticated = value.isAuthenticated
 return (
@@ -49,17 +58,94 @@ return (
                             </li>
                         </ul>
                     </div>
-
+                    {!isAuthenticated ? 
                     <div className="mt-12 lg:mt-0 bg-indigo-500 rounded-3xl border">
                         <Link
-                            to={isAuthenticated ? '/dashboard' : '/login' }
+                            to={'/login' }
                             className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                             >
                             <span className="relative text-sm font-semibold text-white">
-                               {isAuthenticated ? 'Dashboard' : 'Get Started' }
+                               {'Get Started' }
                             </span>
                         </Link>
-                    </div>
+                    </div>: null}
+                    <Menu as='div' className='relative ml-3'>
+                  <div>
+                    <Menu.Button className='flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                      <span className='sr-only'>Open user menu</span>
+                      <img
+                        className='h-8 w-8 rounded-full'
+                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                        alt=''
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter='transition ease-out duration-100'
+                    enterFrom='transform opacity-0 scale-95'
+                    enterTo='transform opacity-100 scale-100'
+                    leave='transition ease-in duration-75'
+                    leaveFrom='transform opacity-100 scale-100'
+                    leaveTo='transform opacity-0 scale-95'
+                  >
+                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                            <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to='dashboard'
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700',
+                            )}
+                          >
+                            Dashboard
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to='dashboard/profile'
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700',
+                            )}
+                          >
+                            Profile
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            to='dashboard/settings'
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700',
+                            )}
+                          >
+                            Settings
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            type='button'
+                            onClick={onLogout}
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700',
+                            )}
+                          >
+                            Sign out
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
                 </div>
             </div>
         </div>
