d532299e9d564ae5af6f72d52dd5ddc6274729a4
alex.mochu <mochualex4@gmail.com>
2023-06-02 14:03:12 +0300

    save search text instate

--- src/search/components/sections/SearchBar.tsx
+++ src/search/components/sections/SearchBar.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react'
 import { countries } from './../../../countries'
+
+const jobInfo = {
+   title: '',
+   country: '',
+   type: '',
+}
+
 export const SearchBar = () => {
+  const [jobState, setJobState] = useState(jobInfo)
+
+  const onChange = e =>
+    setJobState(
+      { ...jobState, [e.target.name]: e.target.value },
+    );
+
   return (
       <div className='flex w-full bg-white h-16 pt-2 px-4 rounded-2xl border-2'>
         <div className='w-[30%] pr-5'>
-            <input className='w-full h-11' placeholder='Job title ....' />
+            <input className='w-full h-11' name='title' value={jobState.title} onChange={onChange} placeholder='Job title ....' />
         </div>
         <div className='w-[30%] pr-5'>
-            <select className='w-full border-none' name="locations" id="locations">
+            <select className='w-full border-none' name="country" id="locations" value={jobState.country} onChange={onChange}>
                 {countries.map((item) => (
-                    <option key={item.country_code} value="country">{item.en_short_name}</option>
+                    <option key={item.country_code} value={item.en_short_name}>{item.en_short_name}</option>
                 ))}
             </select>
         </div>
         <div className='w-[30%] pr-5'>
-            <select className='w-full border-none' name="type" id="type">
+            <select className='w-full border-none' name="type" id="type" value={jobState.type} onChange={onChange}>
                 <option value="remote">Remote</option>
                 <option value="onsite">Onsite</option>
                 <option value="hybrid">Hybrid</option>
