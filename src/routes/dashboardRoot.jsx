c920dd0d9ad3265d824ee47349a0abe43a8c3e29
alex.mochu <mochualex4@gmail.com>
2023-06-12 16:56:16 +0300

    update links

--- src/routes/dashboardRoot.jsx
+++ src/routes/dashboardRoot.jsx
@@ -108,6 +108,16 @@ export default function Dashboard() {
             </svg>
             {!isOpen ?<span className="flex-1 ml-3 whitespace-nowrap">Jobs</span>: null}
          </Link>
+        <Link
+            key={'cover'}
+            to={'/dashboard/cover-letter'}
+            aria-current={'page'}
+            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
+                <path d="M19 4h-2V2H7v2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM7 10h10M7 14h4M12 2v4"></path>
+              </svg>
+            {!isOpen ?<span className="flex-1 ml-3 whitespace-nowrap">Cover Letter</span>: null}
+         </Link>
          {resume ? 
          <Link
               key={'resume-builder'}
@@ -117,16 +127,6 @@ export default function Dashboard() {
                <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
                <span className="flex-1 ml-3 whitespace-nowrap">Resume Builder</span>
           </Link> : null}
-          <Link
-              key={'cover-letter'}
-              to={'/dashboard/cover-letter'}
-              aria-current={'page'}
-              className="flex items-center p-2 pb-8 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
-                <path d="M19 4h-2V2H7v2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM7 10h10M7 14h4M12 2v4"></path>
-              </svg>
-               {!isOpen ? <span className="flex-1 ml-3 whitespace-nowrap">Cover Letter</span>: null}
-          </Link>
          {qa ? 
          <Link
             key={'qa'}
@@ -195,7 +195,7 @@ export default function Dashboard() {
 </aside>
 
 <div className={`p-4 ${isOpen ? 'sm:ml-16' : 'sm:ml-64'}`}>
-   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-20">
+   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-2">
         <Outlet />
    </div>
 </div>
