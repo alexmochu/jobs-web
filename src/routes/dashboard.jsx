5d9b42a4e4101fb328a968df5bf806f19c52f060
alex.mochu <mochualex4@gmail.com>
2023-06-17 22:06:04 +0300

    update  dashboard text

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 h-full mb-4 rounded dark:bg-gray-800">
       <div className="bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10">
         <div className="flex items-center justify-center mb-5 text-3xl font-bold">Jobs Search & Tracker</div>
-        <p className="text-lg">Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia. Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem</p>
+        <p className="text-lg">Revolutionize your job search and effortlessly track applications with our comprehensive AI-powered toolkit. Utilizing advanced OpenAI(gpt) and Claude to not only turbocharge your job search but also provide a built-in application tracker. Stay organized, manage your applications, and gain valuable insights to maximize your chances of success. Experience the power of AI for faster results and streamlined job hunting.</p>
         <div className="flex items-center justify-center">
           <Link
             key={'jobs'}
@@ -18,7 +18,7 @@ export default function Dashboard() {
       </div>
       <div className="bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10">
         <div className="flex items-center justify-center mb-5 text-3xl font-bold">Resume Builder</div>
-        <p className="text-lg">Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia. Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem</p>
+        <p className="text-lg">Elevate your resume to new heights with our AI-driven solution. Crafted by OpenAI(gpt) and Claude, our AI tools analyze industry trends, optimize keywords, and provide expert suggestions to ensure your resume stands out from the crowd. Impress employers with a professionally tailored resume that highlights your skills and achievements, increasing your chances of landing that coveted job interview.</p>
         <div className="flex items-center justify-center">
           <Link
             key={'jobs'}
@@ -31,7 +31,7 @@ export default function Dashboard() {
       </div>
       <div className="bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10">
         <div className="flex items-center justify-center mb-5 text-3xl font-bold">Cover Letter Builder</div>
-        <p className="text-lg">Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia. Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem</p>
+        <p className="text-lg">Leave behind the frustration of writer's block and let our AI technology (OpenAI(gpt) and Claude) generate compelling cover letters for you. Simply provide some basic information, and our AI tools will create personalized, attention-grabbing cover letters that captivate employers. Showcase your unique qualities and experiences effortlessly, giving you a competitive edge in the job application process.</p>
         <div className="flex items-center justify-center">
           <Link
             key={'jobs'}
@@ -44,7 +44,7 @@ export default function Dashboard() {
       </div>
       <div className="bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10">
         <div className="flex items-center justify-center mb-5 text-3xl font-bold">Question & Answer AI Assistant</div>
-        <p className="text-lg">Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia. Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem</p>
+        <p className="text-lg">Prepare to ace your interviews and land your dream job with our AI-powered interview tools. Our OpenAI(gpt) and Claude AI assistants offer interactive question and answer sessions, simulating real interview scenarios. Gain confidence, refine your responses, and enhance your interview skills. Be fully prepared to impress employers and secure the job you desire with our powerful AI interview tools.</p>
         <div className="flex items-center justify-center">
           <Link
             key={'jobs'}
