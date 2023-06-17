44c8aec030ef732bcfb0ff4a26dab54eea73d613
alex.mochu <mochualex4@gmail.com>
2023-06-17 22:25:32 +0300

    update the spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-indigo-600"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-indigo-600"></div>
     </div>
   );
 };
