ce59a9e61e61c660f05361abd9e80dfa95351f2f
alex.mochu <mochualex4@gmail.com>
2023-07-03 21:33:08 +0300

    improve setting UI

--- src/routes/settings.jsx
+++ src/routes/settings.jsx
@@ -6,9 +6,15 @@ export default function Settings() {
       <main>
         <div className='bg-white'>
           <div>
-            <main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-              <div className='flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900'>Settings</h1>
+            <main className='mx-auto px-4 sm:px-6 lg:px-8'>
+              <h1 className='text-4xl font-bold tracking-tight text-gray-900 mb-6'>
+                Settings
+              </h1>
+              <div className='border rounded-lg border-gray-200 p-6'>
+                <p className='mb-2'>Change your password at anytime</p>
+                <button className='border rounded-lg px-4 py-2 bg-gray-50 text-lg font-bold tracking-tight text-gray-900'>
+                  Change password
+                </button>
               </div>
             </main>
           </div>
