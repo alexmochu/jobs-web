75666dcbe93fa6414230e633bba94556e9dd0657
alex.mochu <mochualex4@gmail.com>
2023-07-20 15:21:49 +0300

    delete job modal

---
+++ src/tracker/components/sections/viewJobModalComponents/deleteJob.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import { countries } from '../../../../countries'
+import { jobTypes } from '../../../../jobTypes'
+
+const jobDetails = {
+  jobUrl: 'www.job.com',
+  jobSummary: 'This is a job summary',
+  jobTitle: 'Software Engineer',
+  jobCompany: 'Job Company Ltd'
+}
+
+const jobInfo = {
+  title: 'Kenya',
+  country: 'Kenya',
+  type: 'Kenya',
+}
+
+const jobTypeInfo = {
+    type: 'Remote'
+}
+
+function DeleteJob({setViewState, job}) {
+  const [jobData, setJobData] = useState(jobDetails)
+  const [error, setError] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted value:', jobData);
+    setJobData(jobDetails);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setJobData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+    const [jobState, setJobState] = useState(jobInfo)
+
+    const onChange = (e) => setJobState({ ...jobState, [e.target.name]: e.target.value })
+
+    const [jobType, setJobType] = useState(jobTypeInfo)
+
+    const onJobType = (e) => setJobType({ ...jobType, [e.target.name]: e.target.value })
+
+  const handleDelete = (e) => {
+    console.log('delete job');
+  };
+
+  const handleExit = (e) => {
+    setViewState((prevState) => ({ ...prevState, view: true, edit: false }));
+  };
+
+  return (
+    <>
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">Delete Job</h1>
+    </div>
+      <form onSubmit={handleDelete}>
+        <div className='flex flex-col items-center'>
+          <h1
+            name="jobSummary"
+            className={`block w-full px-3 bg-white text-red-600 text-center`}
+          >
+          {'Are you sure you want to delete this job: '} <span className='text-black'>{job.job_title}</span>{' at '} <span className='text-black'>{job.job_company}</span>{'.'}</h1>
+        <h1
+            name="jobSummary"
+            className={`mb-9 block w-full px-3 bg-white text-red-600 text-center`}
+          >
+          {'This action is irreversible and the item will be permanently removed from the system. Please confirm your decision.'}</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+        <button 
+          type="submit"
+          className='col-span-2 bg-red-600 text-gray-100 pt-2 pb-2 w-full rounded-full tracking-wide
+                                      font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                      shadow-lg'          
+        >Yes</button>
+        <button 
+          type="button"
+          onClick={e => handleExit(e)}
+          className='col-span-2 bg-gray-900 text-gray-100 pt-2 pb-2 w-full rounded-full tracking-wide
+                                      font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                      shadow-lg'          
+        >No</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default DeleteJob
