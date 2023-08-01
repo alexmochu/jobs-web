f6a816ffb4bbeb6d61f45261d3755da38870298f
alex.mochu <mochualex4@gmail.com>
2023-08-01 03:22:41 +0300

    update vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
   define: {
@@ -8,6 +9,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    pluginRewriteAll(),
     // eslintPlugin({
     //   cache: false,
     //   include: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
