7c1851a27969266e3ad794d3911e1c64c2051335
alex.mochu <mochualex4@gmail.com>
2023-08-08 21:10:40 +0300

    update dashboard

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 gap-3 h-full mb-4 rounded dark:bg-gray-800'>
-      <div className='bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10'>
-        <div className='flex items-center justify-center mb-5 text-3xl font-bold'>
+    <div className='relative dark:bg-gray-900 min-h-screen'>
+    <div className='grid grid-cols-1  sm:grid-cols-2 gap-3 mb-4 rounded'>
+      <div className='bg-gray-50 dark:bg-gray-900 h-fit rounded-3xl border-2 px-10 py-10'>
+        <div className='flex items-center dark:text-white justify-center mb-5 text-3xl font-bold'>
           Jobs Search & Tracker
         </div>
-        <p className='text-lg'>
+        <p className='text-lg dark:text-white'>
           Revolutionize your job search and effortlessly track applications with our comprehensive
           AI-powered toolkit. Utilizing advanced OpenAI(gpt) and Claude to not only turbocharge your
           job search but also provide a built-in application tracker. Stay organized, manage your
@@ -19,7 +20,7 @@ export default function Dashboard() {
             key={'jobs'}
             to={'/dashboard/job-tracker'}
             aria-current={'page'}
-            className='flex flex-shrink-0 items-center bg-white text-gray-800 px-2'
+            className='flex dark:bg-gray-900 flex-shrink-0 items-center bg-white text-gray-800 px-2'
           >
             <button className='bg-indigo-500 rounded-3xl px-10 py-3 mt-5 text-white text-lg'>
               View
@@ -27,11 +28,11 @@ export default function Dashboard() {
           </Link>
         </div>
       </div>
-      <div className='bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10'>
-        <div className='flex items-center justify-center mb-5 text-3xl font-bold'>
+      <div className='bg-gray-50 dark:bg-gray-900 h-fit rounded-3xl border-2 px-10 py-10'>
+        <div className='flex items-center dark:text-white justify-center mb-5 text-3xl font-bold'>
           Resume Builder
         </div>
-        <p className='text-lg'>
+        <p className='text-lg dark:text-white'>
           Elevate your resume to new heights with our AI-driven solution. Crafted by OpenAI(gpt) and
           Claude, our AI tools analyze industry trends, optimize keywords, and provide expert
           suggestions to ensure your resume stands out from the crowd. Impress employers with a
@@ -43,7 +44,7 @@ export default function Dashboard() {
             key={'jobs'}
             to={'/dashboard/resume'}
             aria-current={'page'}
-            className='flex flex-shrink-0 items-center bg-white text-gray-800 px-2'
+            className='flex dark:bg-gray-900 flex-shrink-0 items-center bg-white text-gray-800 px-2'
           >
             <button
               disabled
@@ -54,11 +55,11 @@ export default function Dashboard() {
           </Link>
         </div>
       </div>
-      <div className='bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10'>
-        <div className='flex items-center justify-center mb-5 text-3xl font-bold'>
+      <div className='bg-gray-50 dark:bg-gray-900 h-fit rounded-3xl border-2 px-10 py-10'>
+        <div className='flex dark:text-white items-center justify-center mb-5 text-3xl font-bold'>
           Cover Letter Builder
         </div>
-        <p className='text-lg'>
+        <p className='text-lg dark:text-white'>
           Leave behind the frustration of writer's block and let our AI technology (OpenAI(gpt) and
           Claude) generate compelling cover letters for you. Simply provide some basic information,
           and our AI tools will create personalized, attention-grabbing cover letters that captivate
@@ -70,7 +71,7 @@ export default function Dashboard() {
             key={'jobs'}
             to={'/dashboard/cover-letter'}
             aria-current={'page'}
-            className='flex flex-shrink-0 items-center bg-white text-gray-800 px-2'
+            className='flex dark:bg-gray-900 flex-shrink-0 items-center bg-white text-gray-800 px-2'
           >
             <button className='bg-indigo-500 rounded-3xl px-10 py-3 mt-5 text-white text-lg'>
               View
@@ -78,11 +79,11 @@ export default function Dashboard() {
           </Link>
         </div>
       </div>
-      <div className='bg-gray-50 h-fit rounded-3xl border-2 px-10 py-10'>
-        <div className='flex items-center justify-center mb-5 text-3xl font-bold'>
+      <div className='bg-gray-50 dark:bg-gray-900 h-fit rounded-3xl border-2 px-10 py-10'>
+        <div className='flex dark:text-white items-center justify-center mb-5 text-3xl font-bold'>
           Question & Answer AI Assistant
         </div>
-        <p className='text-lg'>
+        <p className='text-lg dark:text-white'>
           Prepare to ace your interviews and land your dream job with our AI-powered interview
           tools. Our OpenAI(gpt) and Claude AI assistants offer interactive question and answer
           sessions, simulating real interview scenarios. Gain confidence, refine your responses, and
@@ -94,7 +95,7 @@ export default function Dashboard() {
             key={'jobs'}
             to={'/dashboard/qa'}
             aria-current={'page'}
-            className='flex flex-shrink-0 items-center bg-white text-gray-800 px-2'
+            className='flex flex-shrink-0 items-center dark:bg-gray-900 bg-white text-gray-800 px-2'
           >
             <button
               disabled
@@ -105,6 +106,7 @@ export default function Dashboard() {
           </Link>
         </div>
       </div>
+    </div>
     </div>
   )
 }
