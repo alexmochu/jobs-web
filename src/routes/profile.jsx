83aeca6a5355faccbcf89d4be54f1684ba9be35c
alex.mochu <mochualex4@gmail.com>
2023-08-08 21:10:50 +0300

    update profile

--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -7,27 +7,27 @@ export default function Profile() {
   return (
     <>
       <Fragment>
-        <main>
-          <div className='bg-white'>
+        <div className='relative dark:bg-gray-900 min-h-screen'>
+          <div className='bg-white dark:bg-gray-900'>
             <div>
               <main className='mx-auto px-4 sm:px-6 lg:px-8'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900 mb-6'>
+                <h1 className='text-4xl dark:text-white font-bold tracking-tight text-gray-900 mb-6'>
                   Profile
                 </h1>
-                <div className='border rounded-lg border-gray-200 p-6'>
+                <div className='border rounded-lg dark:text-white border-gray-200 p-6'>
                   <p className='mb-2'>Your username</p>
-                  <h1 className='text-lg font-bold tracking-tight text-gray-900'>
+                  <h1 className='text-lg dark:text-white font-bold tracking-tight text-gray-900'>
                     @{username}
                   </h1>
                   <p className='mt-6 mb-2'>Your email</p>
-                  <h1 className='text-lg font-bold tracking-tight text-gray-900'>
+                  <h1 className='text-lg dark:text-white font-bold tracking-tight text-gray-900'>
                     {email}
                   </h1>
                 </div>
               </main>
             </div>
           </div>
-        </main>
+        </div>
       </Fragment>
     </>
   )
