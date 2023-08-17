b1ce6183fbd9cfb8fa2570a7a30a33d67bfc5b11
alex.mochu <mochualex4@gmail.com>
2023-08-17 22:58:09 +0300

    which long summary wrap

--- src/tracker/components/sections/viewJobModalComponents/viewJob.jsx
+++ src/tracker/components/sections/viewJobModalComponents/viewJob.jsx
@@ -131,11 +131,11 @@ function ViewJob({setViewState, job}) {
           <p
             name="jobSummary"
             placeholder="Enter job summary"
-            className={`mt-1 mb-4 h-48 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            className={`mt-1 mb-4 h-48 overflow-auto block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                             invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500`}
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500 whitespace-normal`}
           >
             {job.job_description}
         </p>
