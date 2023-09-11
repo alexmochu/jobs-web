79ccacd3e0456b88fb64208783a23b28d020c12d
alex.mochu <mochualex4@gmail.com>
2023-09-11 20:04:46 +0300

    update styling

--- src/components/resume/addResume.jsx
+++ src/components/resume/addResume.jsx
@@ -78,7 +78,7 @@ function CreateResume({closeJobModal, applicationState}) {
         </div>
         <h4 className='mb-2'>Select Template</h4>
         <div className='overflow-y-auto grid grid-cols-2 gap-4 h-[430px]'>
-          <div className={`relative ${template === 1 ? 'text-indigo-500 font-bold border rounded-md shadow-2xl border-indigo-500' : ''}`}>
+          <div className={`relative  opacity-100 ${template === 1 ? 'text-indigo-500 font-bold border rounded-md shadow-2xl border-indigo-500 opacity-50' : ''}`}>
             <img onClick={(e) => selectTemplate(e,1)}  className='border h-[200px] rounded-md shadow-md flex items-center justify-center opacity-100 hover:opacity-50' src="/images/resume/temp-1.png" alt="template-one" />
             <button
               className="absolute mt-5 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-2 bg-blue-500 text-white rounded cursor-pointer"
