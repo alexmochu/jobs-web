f998a155607e0d4620b68c0023125cca26f959a9
alex.mochu <mochualex4@gmail.com>
2023-09-11 19:56:46 +0300

    resume template

---
+++ src/components/resume/resumeTemplate.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function ResumeTemplate() {
+
+  return (
+    <div>
+        <div className='relative'>
+          <img className='border h-fit rounded-md shadow-md flex items-center justify-center' src="/images/resume/temp-1.png" alt="template-one" />
+        </div>
+    </div>
+  )
+}
+
+export default ResumeTemplate
