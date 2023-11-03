56051ac20fc06f19644a3458a407d972d0de59fc
alex.mochu <mochualex4@gmail.com>
2023-11-03 20:25:44 +0300

    add loom video

--- src/landingpage/Features.jsx
+++ src/landingpage/Features.jsx
@@ -1,29 +1,49 @@
 const Features = () => (
   <div id='features'>
     <div className='max-w-7xl mx-auto px-6 md:px-12 xl:px-6 pb-20'>
-      <div className='md:w-2/3 lg:w-1/2'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          className='w-6 h-6 text-secondary'
-        >
-          <path
-            fillRule='evenodd'
-            d='M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z'
-            clipRule='evenodd'
-          />
-        </svg>
+      <div className="md:flex md:space-x-4">
+        <div className='md:w-1/2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            className='w-6 h-6 text-secondary'
+          >
+            <path
+              fillRule='evenodd'
+              d='M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z'
+              clipRule='evenodd'
+            />
+          </svg>
 
-        <h2 className='my-8 text-2xl font-bold text-gray-700 dark:text-white md:text-4xl text-left'>
-          AI technology-first approach to creating winning Resumes
-        </h2>
-        <p className='text-gray-900 dark:text-gray-300 text-xl text-left'>
-          Job Search Revolution: AI-Powered Success at Your Fingertips! Discover Streamlined Search,
-          Resume Wizardry, Cover Letter Mastery, and Interview Excellence. Leverage OpenAI(gpt)
-           to Supercharge Your Journey towards Landing Your Dream Job. Experience the Future
-          of Job Hunting Today!
-        </p>
+          <h2 className='my-8 text-2xl font-bold text-gray-700 dark:text-white md:text-4xl text-left'>
+            AI technology-first approach to creating winning Resumes
+          </h2>
+          <p className='text-gray-900 dark:text-gray-300 text-xl text-left'>
+            Job Search Revolution: AI-Powered Success at Your Fingertips! Discover Streamlined Search,
+            Resume Wizardry, Cover Letter Mastery, and Interview Excellence. Leverage OpenAI(gpt)
+            to Supercharge Your Journey towards Landing Your Dream Job. Experience the Future
+            of Job Hunting Today!
+          </p>
+        </div>
+        <div className='md:w-1/2'>
+<div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
+  <iframe
+    src="https://www.loom.com/embed/de19b04f514841f0b39cbce7ff670490?sid=290873ad-a3cc-40a4-8e30-2126c426e4e0"
+    frameBorder="0"
+    webkitAllowFullScreen
+    mozAllowFullScreen
+    allowFullScreen
+    style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+    }}
+  ></iframe>
+</div>
+        </div>
       </div>
       <div className='mt-16 grid divide-x divide-y divide-gray-100 dark:divide-gray-700 overflow-hidden rounded-3xl border border-gray-100 text-gray-600 dark:border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4'>
         <div className='group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10'>
