bba987001830859d0069d1f4e8867dab50b1721f
alex.mochu <mochualex4@gmail.com>
2023-11-06 18:48:06 +0300

    update about us

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -11,32 +11,32 @@ export default function About() {
                 <h1 className='text-4xl font-bold tracking-tight mb-2 text-gray-900'>About KG Jobs</h1>
               </label>
             </div>
-            <div>
+            <div className='text-xl'>
               Welcome to KG Jobs, your gateway to a brighter future in the job market. At KG Jobs, we believe that finding your dream job should be an empowering and efficient experience. We are your dedicated partner in career success.
 
-Our Vision
+            <h2 className='text-2xl font-bold mt-5'>Our Vision</h2>
 
 KG Jobs, where KG stands for Kejani Garage, is built on the core idea of utilizing cutting-edge AI technology to transform the job search and application process. We understand that the modern job market can be overwhelming, with countless opportunities and challenges. That's why we've harnessed the power of AI to make your journey towards landing your dream job more seamless, efficient, and ultimately successful.
 
-AI-Powered Solutions
+<h2 className='text-2xl font-bold mt-5'>Our AI-Powered Solutions</h2>
 
 At KG Jobs, we are proud to offer AI-powered tools and solutions designed to enhance every aspect of your job search:
 
-1. AI-Driven Resumes: Craft resumes that grab the attention of potential employers. Our AI tools, powered by OpenAI (GPT-3), provide expert guidance to optimize your resume for success.
+<p>1. <span className='font-bold'>AI-Driven Resumes:</span> Craft resumes that grab the attention of potential employers. Our AI tools, powered by OpenAI (GPT-3), provide expert guidance to optimize your resume for success.</p>
 
-2. Winning Cover Letters: Eliminate writer's block with AI-generated, personalized cover letters. Impress employers with compelling introductions.
+<p>2. <span className='font-bold'>Winning Cover Letters:</span> Eliminate writer's block with AI-generated, personalized cover letters. Impress employers with compelling introductions.</p>
 
-3. Interview Excellence: Master the art of interviews with AI-powered tools. OpenAI (GPT-3) offers interactive question and answer sessions to help you confidently ace any job interview.
+<p>3. <span className='font-bold'>Interview Excellence:</span> Master the art of interviews with AI-powered tools. OpenAI (GPT-3) offers interactive question and answer sessions to help you confidently ace any job interview.</p>
 
-Our Technology-First Approach
+<h2 className='text-2xl font-bold mt-5'>Our Technology-First Approach</h2>
 
 KG Jobs is at the forefront of a job search revolution, embracing AI technology as the driving force behind our platform. We leverage state-of-the-art technology stacks, including React (TypeScript), Python (Flask), Langchain (OpenAI Agent), and PostgreSQL, to create a seamless and user-friendly experience.
 
-Join Us on this Journey
+<h2 className='text-2xl font-bold mt-5'>Join Us on this Journey</h2>
 
 KG Jobs is not just a platform; it's a community. We invite you to join us on this incredible journey as we strive to make job searching and interviews easier, more efficient, and ultimately, more successful. We share our progress, insights, and challenges as we continue to innovate and improve our services.
 
-Unlock Your Potential
+<h2 className='text-2xl font-bold mt-5'>Unlock Your Potential</h2>
 
 At KG Jobs, we are dedicated to helping you unlock your full potential and achieve your career aspirations. With our AI-powered solutions, you can streamline your job search, create standout resumes and cover letters, and confidently navigate job interviews. The future of job hunting is here, and it's at your fingertips with KG Jobs.
 
